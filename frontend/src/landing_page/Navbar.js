f0db8a4dbd528d932122df9c8cf4b561d19afe15
PsyKickGuy <shubhdas2005@gmail.com>
2025-07-12 01:39:56 +0530

    footer, navbar, openacc

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,7 +1,61 @@
 import React from "react";
 
 function Navbar() {
-  return <h1>Navbar</h1>;
+  return (
+    <nav
+      class="navbar navbar-expand-lg border-bottom"
+      style={{ backgroundClip: "#FFF" }}
+    >
+      <div class="container p-2">
+        <a class="navbar-brand" href="#">
+          {/* <img src="media/images/logo.svg" alt="Logo" /> */}
+          <h1 style={{ width: "25%" }}>StonxHub</h1>
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="d-flex" role="search">
+            <ul class="navbar-nav mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  SignUp
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Product
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Support
+                </a>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
